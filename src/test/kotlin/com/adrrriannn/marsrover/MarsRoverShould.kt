d666a4e2cc0e6d12edf8da6d8b5b1c5171fe39aa
adrrriannn <adrian.romero@wallapop.com>
2021-03-27 11:26:58 +0100

    Added test for move forward facing east

--- src/test/kotlin/com/adrrriannn/marsrover/MarsRoverShould.kt
+++ src/test/kotlin/com/adrrriannn/marsrover/MarsRoverShould.kt
@@ -17,7 +17,7 @@ class MarsRoverShould {
     private val marsRover = MarsRover(scanner, printer)
 
     @Test
-    fun `move forward`() {
+    fun `move forward facing north`() {
         val horizontalSize = 5
         val verticalSize = 5
 
@@ -45,6 +45,37 @@ class MarsRoverShould {
         marsRover()
 
         assertRoverExpectedPositionWasPrinted("Rover is at x:0 y:1 facing:n")
+    }
+
+    @Test
+    fun `move forward facing east`() {
+        val horizontalSize = 5
+        val verticalSize = 5
+
+        val initialHorizontalPosition = 0
+        val initialVerticalPosition = 0
+        val initialDirection = "e"
+        val command = "f"
+
+        willPrint("Insert horizontal map size:")
+        willPrint("Insert vertical map size:")
+        willPrint("Insert horizontal initial rover position:")
+        willPrint("Insert vertical initial rover position:")
+        willPrint("Insert initial rover direction:")
+        willPrint("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
+        willPrint("Rover is at x:1 y:0 facing:e")
+
+        willReturnHorizontalMapSize(horizontalSize)
+            .willReturnVerticalMapSize(verticalSize)
+            .willReturnRoverInitialHorizontalPosition(initialHorizontalPosition)
+            .willReturnRoverInitialVerticalPosition(initialVerticalPosition)
+
+        willReturnRoverInitialDirection(initialDirection)
+            .willReturnCommand(command)
+
+        marsRover()
+
+        assertRoverExpectedPositionWasPrinted("Rover is at x:1 y:0 facing:e")
     }
 
     private fun willPrint(message: String) {
