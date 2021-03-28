8c2e91dfce6663facfefcd37946789ea56690d1a
adrrriannn <adrian.romero@wallapop.com>
2021-03-28 12:50:22 +0200

    Added rotate command classes

--- src/main/kotlin/com/adrrriannn/marsrover/adapter/MarsRoverConsoleRunner.kt
+++ src/main/kotlin/com/adrrriannn/marsrover/adapter/MarsRoverConsoleRunner.kt
@@ -2,6 +2,8 @@ package com.adrrriannn.marsrover.adapter
 
 import com.adrrriannn.marsrover.context.move.MoveBackwardCommand
 import com.adrrriannn.marsrover.context.move.MoveForwardCommand
+import com.adrrriannn.marsrover.context.rotate.RotateLeftCommand
+import com.adrrriannn.marsrover.context.rotate.RotateRightCommand
 import java.io.PrintStream
 import java.util.Scanner
 
@@ -18,54 +20,28 @@ class MarsRoverConsoleRunner(private val reader: Scanner,
         printer.println("Insert vertical initial rover position:")
         var roverInitialPositionY = reader.nextInt()
         printer.println("Insert initial rover direction:")
-        var roverInitialPosition = reader.next() //n = north, e = east, w = west, s = south
+        var roverInitialDirection = reader.next() //n = north, e = east, w = west, s = south
 //        do {
             printer.println("Insert command (f = forward, b = backward, l = turn left, r = turn right):")
             val inputCommand = reader.next()
 
             if (inputCommand == "f") {
-                val coordinates = MoveForwardCommand().move(roverInitialPosition, roverInitialPositionY, mapYSize, roverInitialPositionX, mapXSize)
+                val coordinates = MoveForwardCommand().move(roverInitialDirection, roverInitialPositionY, mapYSize, roverInitialPositionX, mapXSize)
                 roverInitialPositionX = coordinates.x
                 roverInitialPositionY = coordinates.y
             }
             if (inputCommand == "b") {
-                val coordinates = MoveBackwardCommand().move(roverInitialPosition, roverInitialPositionY, mapYSize, roverInitialPositionX, mapXSize)
+                val coordinates = MoveBackwardCommand().move(roverInitialDirection, roverInitialPositionY, mapYSize, roverInitialPositionX, mapXSize)
                 roverInitialPositionX = coordinates.x
                 roverInitialPositionY = coordinates.y
             }
             if (inputCommand == "l") {
-                when (roverInitialPosition) {
-                    "n" -> {
-                        roverInitialPosition = "w"
-                    }
-                    "w" -> {
-                        roverInitialPosition = "s"
-                    }
-                    "s" -> {
-                        roverInitialPosition = "e"
-                    }
-                    "e" -> {
-                        roverInitialPosition = "n"
-                    }
-                }
+                roverInitialDirection = RotateLeftCommand().rotate(roverInitialDirection)
             }
             if (inputCommand == "r") {
-                when (roverInitialPosition) {
-                    "n" -> {
-                        roverInitialPosition = "e"
-                    }
-                    "e" -> {
-                        roverInitialPosition = "s"
-                    }
-                    "s" -> {
-                        roverInitialPosition = "w"
-                    }
-                    "w" -> {
-                        roverInitialPosition = "n"
-                    }
-                }
+                roverInitialDirection = RotateRightCommand().rotate(roverInitialDirection)
             }
-            printer.println(String.format("Rover is at x:%d y:%d facing:%s", roverInitialPositionX, roverInitialPositionY, roverInitialPosition))
+            printer.println(String.format("Rover is at x:%d y:%d facing:%s", roverInitialPositionX, roverInitialPositionY, roverInitialDirection))
 //        } while (true)
     }
 
